7fbccec01a603d91221b49d458d6feb609423859
TheElementOFLif3 <116418444+TheElementOFLif3@users.noreply.github.com>
2022-12-23 10:35:01 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        // r – poluprečnik osnove valjka
-        // H – visina
-        // Pi – konstanta Pi, 3.14
+       // r – radius of the base of the roller
+        // H – height
+        // Pi – constant Pi, 3.14
         // V = r2 * Pi * H
-
         int r = 10;
         int H = 50;
         double Pi = 3.14;
