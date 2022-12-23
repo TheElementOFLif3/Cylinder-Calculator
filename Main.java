e3fa8346e3560ec48abfc95eb2e014989df071eb
TheElementOFLif3 <116418444+TheElementOFLif3@users.noreply.github.com>
2022-12-23 10:34:03 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // r – poluprečnik osnove valjka
+        // H – visina
+        // Pi – konstanta Pi, 3.14
+        // V = r2 * Pi * H
+
+        int r = 10;
+        int H = 50;
+        double Pi = 3.14;
+        double Volume = r * r * Pi * H;
+        System.out.printf("Zapremina valjka rezultat="+Volume);
+
+    }
+}
