4afff59edcdccdb8cad8ef07659e12ccdcc375eb
TheElementOFLif3 <116418444+TheElementOFLif3@users.noreply.github.com>
2022-12-23 10:37:15 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-       // r – radius of the base of the roller
+       // r – radius of the base of the cylinder
         // H – height
         // Pi – constant Pi, 3.14
         // V = r2 * Pi * H
@@ -9,7 +9,7 @@ public class Main {
         int H = 50;
         double Pi = 3.14;
         double Volume = r * r * Pi * H;
-        System.out.printf("Zapremina valjka rezultat="+Volume);
+        System.out.printf("Volume of the cylinder="+Volume);
 
     }
 }
